76221befbbab55bd4f95c54891226ab38779f53a
rizwan104nb <rizwansgd4@gmail.com>
2026-04-22 06:13:49 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,6 +17,6 @@ self.addEventListener("install", e=>{
 
 self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
